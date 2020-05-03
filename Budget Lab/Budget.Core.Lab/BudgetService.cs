fdae8d2757f3d2f49f2d115de6c9ece5aa051020
Marsen <thisismysoul@gmail.com>
2020-05-03 15:55:10 +0800

    logic group

--- Budget Lab/Budget.Core.Lab/BudgetService.cs
+++ Budget Lab/Budget.Core.Lab/BudgetService.cs
@@ -24,15 +24,6 @@ namespace Budget.Core.Lab
             var startBudget = GetBudget(start);
             var startAmount = startBudget?.Amount ?? 0;
             decimal startOneDay = startAmount / startMonthDays;
-
-            var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
-            if (diffMonth < 2)
-            {
-                //// 當月超過1日
-                var diffDays = end.Subtract(start).TotalDays + 1;
-                return (decimal) diffDays * startOneDay;
-            }
-
             var s = (startMonthDays - start.Day + 1) * startOneDay;
 
             var endMonthDays = DateTime.DaysInMonth(end.Year, end.Month);
@@ -40,6 +31,14 @@ namespace Budget.Core.Lab
             var endAmount = endBudget?.Amount ?? 0;
             decimal endOneDay = endAmount / endMonthDays;
             var e = end.Day * endOneDay;
+            var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
+
+            if (diffMonth < 2)
+            {
+                //// 當月超過1日
+                var diffDays = end.Subtract(start).TotalDays + 1;
+                return (decimal) diffDays * startOneDay;
+            }
 
             var tmpMid = 0m;
             for (var i = 1; i < diffMonth - 1; i++)
